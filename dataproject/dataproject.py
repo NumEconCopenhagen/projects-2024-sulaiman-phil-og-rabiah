2e3ce686b9281bdc01cb3ff4e5987563e6fdc2a1
bmq911 <bmq911@alumni.ku.dk>
2024-04-09 17:31:28 +0200

    Alt d 9 april

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,16 +1,31 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
+# A1 - First data set
 
-    Args:
-        df (pd.DataFrame): pandas dataframe 
+# a. load
+nah1_api = DstApi('NAH1') 
+params = nah1_api._define_base_params(language='en')
+nah1_true = nah1_api.get_data(params=params)
 
-    Returns:
-        df (pd.DataFrame): pandas dataframe
+# b. rename and replace
+nah1_true.rename(columns=columns_dict,inplace=True)
 
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
+# c. replace data
+for key,value in var_dict.items():
+   nah1_true.variable.replace(key,value,inplace=True)
+
+for key,value in unit_dict.items():
+   nah1_true.unit.replace(key,value,inplace=True)
+
+# d. keep if in var_dict
+I = False
+for key,value in var_dict.items():
+    I = I | (nah1_true.variable == value)
+nah1_true = nah1_true[I]
+   
+# e. convert values to numeric
+nah1_true.value = nah1_true.value.astype('float')
+
+# d. summary statistics
+nah1_true.groupby(['variable','unit']).describe()
+
+# f. Sort by year 
+nah1_true.sort_values(by='year',inplace=True)
