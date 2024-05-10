42557d43aabfd35d29bfc5b60f47e4508eae555b
bmq911 <bmq911@alumni.ku.dk>
2024-05-10 17:07:15 +0200

    hej

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,28 +1,62 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-import pandas_datareader # install with `pip install pandas-datareader`
-from dstapi import DstApi # install with `pip install git+https://github.com/alemartinello/dstapi`
+def plot_inflation(data):
+    plt.figure(figsize=(12, 8))
+    plt.plot(data['Year'], data['Inflation rate'], marker='o', linestyle='-', color='blue', label='Inflation')
+    plt.title('Inflation from 2007 to 2022')
+    plt.xlabel('Year')
+    plt.ylabel('Inflation Percentage')
+    plt.xticks(data['Year'].unique(), rotation=45)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-# user written modules
-import dataproject as dp
+import matplotlib.pyplot as plt
 
-def inf_fig(x, y, color='blue', label='y', legend_pos='best'):
-  
-  Returns: 
-  matplotlib.figure.Figure: 
-  
-plt.figure(figsize=(12, 8))
-plt.plot(PRIS111['Year'], PRIS111['Inflation rate'], marker='o', linestyle='-', color='blue', label='Inflation')
-plt.title('Inflation from 2007 to 2022')
-plt.xlabel('Year')
-plt.ylabel('Inflation Percentage')
-plt.xticks(PRIS111['Year'].unique(), rotation=45)
-plt.legend()
-plt.grid(True)
-plt.show();
+def plot_merged_data(Combined_data):
+    plt.figure(figsize=(12, 8))
+    plt.plot(Combined_data['Year'], Combined_data['Inflation rate'], marker='o', linestyle='-', color='blue', label='Inflation')
+    plt.plot(Combined_data['Year'], Combined_data['Unemployment'], marker='x', linestyle='--', color='green', label='Unemployment')
+    plt.title('Inflation and Unemployment from 2007 to 2022')
+    plt.xlabel('Year')
+    plt.ylabel('Percentage')
+    plt.xticks(Combined_data['Year'].unique(), rotation=45)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 
+import matplotlib.pyplot as plt
 
+def plot_real_gdp(GDP):
+    plt.figure(figsize=(10, 4))
+    plt.plot(GDP['Year'], GDP['Real GDP'], marker='o', color='blue')
+    plt.ylabel('Real GDP (Mia. kr.)')
+    plt.title('Real GDP Over Time in Denmark')
+    plt.xlabel('Year')
+    plt.grid(True)  # Add gridlines
+    plt.tight_layout()
+    plt.show()
 
+def plot_graph(x, y, title='title', xlabel='xlabel', ylabel='ylabel', label_a='label_a'):
+    """ docstrings """
+    plt.figure(figsize=(12, 8))
+    plt.plot(x, y, marker='o', linestyle='-', color='blue', label=label_a)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def plot_graph_2(x1,x2, y1,y2, title='title', xlabel='xlabel', ylabel='ylabel', label_a='label_a', label_b='label_b'):
+    """ docstrings """
+    plt.figure(figsize=(12, 8))
+    plt.plot(x1, y1, marker='o', linestyle='-', color='blue', label=label_a)
+    plt.plot(x2, y2, marker='x', linestyle='--', color='green', label=label_b)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
