401eac86450ed8715c72a319223b2f4ebd9d58cb
bmq911 <bmq911@alumni.ku.dk>
2024-06-10 11:57:56 +0200

    hehe

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,5 +1,20 @@
 import matplotlib.pyplot as plt
 
+def plot_graph_j(x1, y1, y2=None, title='title', xlabel='xlabel', ylabel='ylabel', label_a='label_a', label_b='label_b'):
+    """ docstrings """
+    plt.figure(figsize=(8, 4))
+    plt.plot(x1, y1, marker='o', linestyle='-', color='blue', label=label_a)
+    if y2 is not None:
+        plt.plot(x1, y2, marker='x', linestyle='--', color='green', label=label_b)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.grid(True)
+    plt.show();
+
+
+
 def plot_graph(x, y, title='title', xlabel='xlabel', ylabel='ylabel', label_a='label_a'):
     """ docstrings """
     plt.figure(figsize=(8, 4))
