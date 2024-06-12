c63ba59d1a7d6f889ee120ea46de96ec938ff3ae
bmq911 <bmq911@alumni.ku.dk>
2024-06-12 15:58:00 +0200

    hehee

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -15,16 +15,11 @@ def plot_graph_j(x1, y1, y2=None, title='title', xlabel='xlabel', ylabel='ylabel
 
 
 
-def plot_graph(x, y, title='title', xlabel='xlabel', ylabel='ylabel', label_a='label_a'):
-    """ docstrings """
-    plt.figure(figsize=(8, 4))
-    plt.plot(x, y, marker='o', linestyle='-', color='blue', label=label_a)
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+
+
+
+
+
 
 def plot_graph_2(x1,x2, y1,y2, title='title', xlabel='xlabel', ylabel='Percentage', label_a='label_a', label_b='label_b'):
     """ docstrings """
