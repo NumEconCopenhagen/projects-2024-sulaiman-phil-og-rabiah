e5dbe5fd8306d2a2b48400a0ccd2563c6080a682
bmq911 <bmq911@alumni.ku.dk>
2024-06-20 12:36:39 +0200

    Rettelserr

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
 
 
-def plot_graph_j(x1, y1, y2=None, title='title', xlabel='xlabel', ylabel='ylabel', label_a='label_a', label_b='label_b'):
+def plot_graph_j(x1, y1, title='title', xlabel='xlabel', ylabel='ylabel', label_a='label_a', label_b='label_b'):
     """
-    Plots a graph with one or two sets of y-values against a common set of x-values.
+      Plots a simple line graph with the provided data and labels.
 
     Parameters:
     x1 (list or array-like): X-axis values.
     y1 (list or array-like): Y-axis values for the first plot.
-    y2 (list or array-like, optional): Y-axis values for the second plot. Default is None.
     title (str, optional): Title of the graph. Default is 'title'.
     xlabel (str, optional): Label for the X-axis. Default is 'xlabel'.
     ylabel (str, optional): Label for the Y-axis. Default is 'ylabel'.
@@ -18,17 +17,16 @@ def plot_graph_j(x1, y1, y2=None, title='title', xlabel='xlabel', ylabel='ylabel
     Returns:
     None
 
-    This function creates a line plot of the given x and y values. If a second set of y values is provided,
-    it plots both sets on the same graph with different styles and labels. The function also adds a title,
+    This function creates a line plot of the given x and y values. The function also adds a title,
     axis labels, a legend, and a grid to the plot.
     """
     plt.figure(figsize=(8, 4))
     plt.plot(x1, y1, marker='o', linestyle='-', color='blue', label=label_a)
-    if y2 is not None:
-        plt.plot(x1, y2, marker='x', linestyle='--', color='green', label=label_b)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.legend()
     plt.grid(True)
     plt.show()
+
+
